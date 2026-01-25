a0d5b02c700cd46c2991ea6dc650cf91bdb7e258
Tom Barbalet <barbalet@gmail.com>
2026-01-25 09:54:16 -0800

    Update command

--- src/cli.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import { runCli } from "./cli_main.js";
-
-runCli(process.argv.slice(2)).catch((err) => {
-  console.error(err?.stack ?? String(err));
-  process.exitCode = 1;
-});
